39c26d6eb48c5190f62c4ec3357ebfdf58e3468c
noname for the moment <70998165+eugengh@users.noreply.github.com>
2022-10-08 12:14:58 +0300

    Add files via upload

---
+++ sumcifre.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+void sumcif(int n, int &s)
+
+{
+
+    if (n==0)
+
+       s = 0;
+
+    else {
+
+        sumcif((n/10),s);
+
+        s = s +(n%10);
+
+       
+    }
+
+        
+        
+}
+
+int main()
+ {
+
+    int s;
+
+    sumcif(29, s);
+
+    cout<<s;
+
+    return 0;
+
+}
